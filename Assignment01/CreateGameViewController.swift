e9f7348492238b58a0cc8902c0349d7c40e15904
Yiping Guo <s3177105@student.rmit.edu.au>
2017-01-12 19:28:16 +1100

    Created first page without skin

---
+++ Assignment01/CreateGameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateGameViewController.swift
+//  Assignment01
+//
+//  Created by Yiping Guo on 12/01/2017.
+//  Copyright © 2017 au.rmit.s3177105. All rights reserved.
+//
+
+import UIKit
+
+class CreateGameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
