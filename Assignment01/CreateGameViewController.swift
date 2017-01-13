71ac4854c8aefdb67d21a91c19e8fb478852d836
Yiping Guo <s3177105@student.rmit.edu.au>
2017-01-13 17:36:30 +1100

    Fixed nav controller on the first and second page

--- Assignment01/CreateGameViewController.swift
+++ Assignment01/CreateGameViewController.swift
@@ -13,14 +13,26 @@ class CreateGameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /*
         let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: 414, height: 64))
         self.view.addSubview(navBar)
         let navItem = UINavigationItem(title: "Setup")
-        let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: nil, action: #selector(getter: UIAccessibilityCustomAction.selector))
-        navItem.rightBarButtonItem = doneItem
+        let backItem = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.plain, target: self, action: #selector(CreateGameViewController.back(sender:)))
+        //let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: nil, action: #selector(CreateGameViewController.done(sender:)))
+        navItem.leftBarButtonItem = backItem
+        //navItem.rightBarButtonItem = doneItem
         navBar.setItems([navItem], animated: false)
-        
+            */
     }
+    
+    /*
+    func back(sender: UIBarButtonItem) {
+        // perform the custom action
+        // ...
+        // go back to the previous ViewController
+        let _ = navigationController?.popViewController(animated: true)
+    }
+    */
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
